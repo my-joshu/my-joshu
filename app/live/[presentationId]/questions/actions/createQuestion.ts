6b5f865a515844d2244ebcda3c7cd879e8b0bcc4
Yuta Kusuno <yutakusuno51@gmail.com>
2024-07-04 09:45:33 -0700

    Create anonymous sign in process and q&a session page

---
+++ app/live/[presentationId]/questions/actions/createQuestion.ts
@@ -0,0 +1,92 @@
+"use server";
+
+import { z } from "zod";
+import { Tables } from "@/types/supabase";
+import { createClient } from "@/utils/supabase/server";
+
+const QuestionSchema = z.object({
+  presentationId: z.number(),
+  attendeeId: z.number(),
+  content: z
+    .string()
+    .trim()
+    .min(1, "Please enter your question")
+    .max(160, "Too many characters"),
+});
+
+async function validateInput(
+  presentationId: number,
+  attendeeId: number,
+  formData: FormData
+) {
+  const validationResult = QuestionSchema.safeParse({
+    presentationId,
+    attendeeId,
+    content: formData.get("content"),
+  });
+
+  if (!validationResult.success) {
+    console.error(validationResult.error.flatten().fieldErrors);
+    throw new Error("Missing fields to create question");
+  }
+
+  return validationResult.data;
+}
+
+export async function createQuestion(
+  presentationId: number,
+  attendeeId: number,
+  prevState: {
+    resetKey: string;
+    message: string;
+  },
+  formData: FormData
+) {
+  const supabase = createClient();
+
+  try {
+    const { content } = await validateInput(
+      presentationId,
+      attendeeId,
+      formData
+    );
+
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+
+    if (!user || !user.is_anonymous) {
+      throw new Error("Anonymous user not found");
+    }
+
+    const { data, error } = await supabase
+      .from("questions")
+      .insert({
+        presentation_id: presentationId,
+        attendee_id: attendeeId,
+        content,
+      })
+      .select()
+      .returns<Tables<"questions">[]>();
+
+    if (error) {
+      throw new Error("Poor internet communication");
+    }
+    // TODO Integrate Supabase realtime to update Q&A list
+
+    return {
+      resetKey: data[0].uuid,
+      message: `Created question ${data[0].content}`,
+    };
+  } catch (error) {
+    let message = "Failed to create question";
+    if (error instanceof Error) {
+      message += `: ${error.message}`;
+    }
+
+    return {
+      resetKey: prevState.resetKey,
+      message: message,
+    };
+  }
+}
