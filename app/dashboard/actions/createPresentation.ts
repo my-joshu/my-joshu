c38a754d4063c1bf204ae512eb754c232dc42e25
samuraikun <tazitaziawawa@gmail.com>
2024-07-09 12:59:00 -0700

    cosmetic

--- app/dashboard/actions/createPresentation.ts
+++ app/dashboard/actions/createPresentation.ts
@@ -54,7 +54,7 @@ export async function createPresentation(
     const endTime = formData.get("endTime")
       ? parseISO(formData.get("endTime") as string)
       : null;
-    const { data, error } = await supabase
+    const { data } = await supabase
       .from("presentations")
       .insert({
         speaker_id: speakerId,
@@ -64,21 +64,20 @@ export async function createPresentation(
         end_time: endTime,
       })
       .select("*")
-      .returns<Tables<"presentations">[]>();
+      .single<Tables<"presentations">>();
 
-    if (error) {
-      throw error;
+    if (!data) {
+      throw new Error("Failed to create presentation");
     }
-    const presentation = data[0];
 
     // NOTE: This URL is to be able to join the presentation for audience
-    const presentationUrl = `${process.env.NEXT_PUBLIC_SUPABASE_URL}/auth/anonymous?presentationId=${presentation.id}`;
+    const presentationUrl = `${process.env.NEXT_PUBLIC_SUPABASE_URL}/auth/anonymous?presentationId=${data.id}`;
     const qrCode = await generateQR(presentationUrl)
 
     await supabase
       .from("presentations")
       .update({ qr_code: qrCode })
-      .eq("id", presentation.id);
+      .eq("id", data.id);
 
     console.log("Created presentation:", data);
   } catch (error) {
